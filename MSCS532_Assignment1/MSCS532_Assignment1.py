27975f5ec3b23043b443b58b2d734bab36932fa4
nferradas1998 <nbertoli32572@ucumberlands.edu>
2025-01-17 00:44:47 -0800

    adding coments for sorting visibility

--- MSCS532_Assignment1/MSCS532_Assignment1.py
+++ MSCS532_Assignment1/MSCS532_Assignment1.py
@@ -1,7 +1,9 @@
 
 def insertion_sort_monotonically_descending(numArr):
+    print("initial array", numArr)
     for i in range(1, len(numArr)):
         current = numArr[i]
+        print("current value being sorted: ",current, "at position ", i)
         position = i - 1
 
         while position >= 0 and numArr[position] < current:
@@ -9,5 +11,10 @@ def insertion_sort_monotonically_descending(numArr):
             position -= 1
 
         numArr[position + 1] = current
+        print("new order for the array after key was sorted: ", numArr)
+
     
     return numArr
+
+arr = [2, 5, 9, 1, 3,6, 1, 0]
+print("sorted array: ", insertion_sort_monotonically_descending(arr))
