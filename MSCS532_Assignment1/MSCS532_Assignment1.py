7f481f491da46548948fff37bfa5b0f0d3809a37
nferradas1998 <nbertoli32572@ucumberlands.edu>
2025-01-17 00:39:55 -0800

    creating method for insertion sort

--- MSCS532_Assignment1/MSCS532_Assignment1.py
+++ MSCS532_Assignment1/MSCS532_Assignment1.py
@@ -1,1 +1,13 @@
 
+def insertion_sort_monotonically_descending(numArr):
+    for i in range(1, len(numArr)):
+        current = numArr[i]
+        position = i - 1
+
+        while position >= 0 and numArr[position] < current:
+            numArr[position + 1] = numArr[position]
+            position -= 1
+
+        numArr[position + 1] = current
+    
+    return numArr
